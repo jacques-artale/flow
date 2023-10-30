d790b1b314df5a9fcd3fc18237e8accd5c1c260e
boblee <jacques.artale@hotmail.com>
2023-10-30 23:55:52 +0100

    fix: some UI issues and allows user to clear a path when clicking on the connected endpoint

--- client/src/components/puzzle.js
+++ client/src/components/puzzle.js
@@ -4,12 +4,14 @@ import Cell from './grid_components/cell';
 function Puzzle({ grid_data }) {
 
   const [current_grid, set_current_grid] = useState([[]]);
-  const [active_color, set_active_color] = useState();
+  const [active_endpoint, set_active_endpoint] = useState();
+
+  const directions = [[0, 1], [1, 0], [0, -1], [-1, 0]];
 
   useEffect(() => {
     // wipe out all paths, leaving only endpoints
     const endpoint_grid = grid_data.map(row => row.map(cell => {
-      return cell.type === 'path' ? { ...cell, type: 'empty', color: '' } : cell;
+      return cell.type === 'path' ? { ...cell, type: 'empty', color: '', id: '' } : cell;
     }));
     set_current_grid(endpoint_grid);
   }, [grid_data]);
@@ -24,29 +26,86 @@ function Puzzle({ grid_data }) {
     };
   }, []);
 
-  function choose_color(cell) {
-    if (cell.type !== 'empty') set_active_color(cell.color);
+  function choose_color(row_index, col_index) {
+    const cell = current_grid[row_index][col_index];
+    if (cell.type !== 'empty') set_active_endpoint(cell);
+    if (cell.type !== 'empty') clear_path(row_index, col_index);
   }
   
+  /**
+   * Sets the active endpoint to null, which means that the user is no longer drawing a path.
+   */
   function release_color() {
-    set_active_color(null);
+    set_active_endpoint(null);
   }
 
+  /**
+   * Adds a cell to the path if it is empty will be connected to the path.
+   * @param {number} row_index
+   * @param {number} col_index
+   */
   function add_to_path(row_index, col_index) {
-    if (active_color === null) return;
+    if (active_endpoint === null) return;
+
+    // make sure that new cell is adjacent to a cell of the same path
+    // (to avoid adding path cells without being connected to the endpoint)
+    let adjacent_cells = 0;
+    for (const direction of directions) {
+      let new_row = row_index + direction[0];
+      let new_col = col_index + direction[1];
+
+      if (new_row < 0 || new_row >= current_grid.length || new_col < 0 || new_col >= current_grid[0].length) continue;
+      if (current_grid[new_row][new_col].id === active_endpoint.id) adjacent_cells++;
+    }
+    if (adjacent_cells === 0) return;
 
     set_current_grid(prev_grid => {
       // Targeted deep copy for optimization
       const new_grid = [...prev_grid];
       new_grid[row_index] = [...new_grid[row_index]];
-      new_grid[row_index][col_index] = { ...new_grid[row_index][col_index] };
-
-      if (new_grid[row_index][col_index].type !== 'endpoint') {
-        new_grid[row_index][col_index].color = active_color;
-        new_grid[row_index][col_index].type = 'path';
+    
+      if (new_grid[row_index][col_index].type === 'empty') {
+        new_grid[row_index][col_index] = { ...active_endpoint, type: 'path' };
       }
       return new_grid;
     });
+  }
+
+  /**
+   * Clears the entire path from the board.
+   * @param {number} row_index
+   * @param {number} col_index
+   */
+  function clear_path(row_index, col_index) {
+    // breadth first search to find all cells in the path
+    const new_grid = JSON.parse(JSON.stringify(current_grid));
+    const target_id = new_grid[row_index][col_index].id;
+
+    const type = new_grid[row_index][col_index].type;
+    
+    if (type === 'path') return; // TODO: Implement clearing of path cells from this point forward
+
+    const queue = [[row_index, col_index]];
+
+    while (queue.length > 0) {
+      const [row, col] = queue.shift();
+      if (new_grid[row][col].type === 'path') {
+        new_grid[row][col] = { ...new_grid[row][col], type: 'empty', color: '', id: '' };
+      }
+      
+      for (const direction of directions) {
+        let new_row = row + direction[0];
+        let new_col = col + direction[1];
+
+        if (new_row < 0 || new_row >= new_grid.length || new_col < 0 || new_col >= new_grid[0].length) continue;
+        if (new_grid[new_row][new_col].id !== target_id) continue;
+        if (new_grid[new_row][new_col].type === 'path') {
+          queue.push([new_row, new_col]);
+        }
+      }
+    }
+
+    set_current_grid(new_grid);
   }
 
   return (
@@ -65,7 +124,7 @@ function Puzzle({ grid_data }) {
               <div
                 style={{width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center', display: 'flex'}}
                 onDragStart={(event) => event.preventDefault()}
-                onMouseDown={() => choose_color(cell)}
+                onMouseDown={() => choose_color(row_index, col_index)}
                 onMouseEnter={() => add_to_path(row_index, col_index)}
               >
                 <Cell grid_data={current_grid} cell={cell} row={row_index} col={col_index} color={cell.color} />
