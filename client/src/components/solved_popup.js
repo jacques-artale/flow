631830b9e486d53c8948fbd58c6b236bae15f64e
boblee <jacques.artale@hotmail.com>
2024-01-04 22:05:22 +0100

    fix: display time taken to solve

--- client/src/components/solved_popup.js
+++ client/src/components/solved_popup.js
@@ -2,13 +2,26 @@ import React, { useEffect, useRef } from 'react';
 import '../App.css';
 import get_style from "../style/style";
 
+function get_solved_time_string(solve_time) {
+  const hours = Math.floor(solve_time / 3600);
+  const minutes = Math.floor((solve_time % 3600) / 60);
+  const seconds = Math.floor(solve_time % 60);
+
+  let time_string = '';
+  if (hours > 0) time_string += `${hours}h `;
+  if (minutes > 0) time_string += `${minutes}m `;
+  time_string += `${seconds}s`;
+
+  return time_string;
+}
+
 function SolvedPopup({ solved, solve_time }) {
   const style = get_style();
 
   return (
     <div style={{...style.solved_overlay, display: solved ? 'flex' : 'none'}}>
       <h2>Puzzle Solved</h2>
-      <p>Time: {solve_time} seconds</p>
+      <p>Time taken: {get_solved_time_string(solve_time)}</p>
     </div>
   );
 }
